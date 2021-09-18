cf125b591a0c3798fec5e5b53ccc457b3a5e97e7
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2021-09-18 14:22:45 -0400

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
-  return <>please work</>;
+  return <BrowserRouter></BrowserRouter>;
 }
