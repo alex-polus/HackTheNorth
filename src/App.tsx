fadf7e5117057c6db934c0a13be9630c97f8ae78
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2021-09-18 14:27:38 -0400

    router complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Router } from "react-router-dom";
 
 export default function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return <Router></Router>;
 }
