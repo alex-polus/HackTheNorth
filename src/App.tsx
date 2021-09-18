a8ff3044bddac3e9e034376c0a033150f2cc5c21
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2021-09-18 15:25:32 -0400

    remove warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router } from "react-router";
+import { Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
@@ -7,8 +7,8 @@ import Home from "./Pages/Home";
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Home} />
-      <Route path="/" component={About} />
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
     </BrowserRouter>
   );
 }
