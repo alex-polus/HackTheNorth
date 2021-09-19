3a1c3acc4b34088097850e56d73b9fee972ed8f1
JugalC <44619729+JugalC@users.noreply.github.com>
2021-09-18 23:56:33 -0400

    added page 2 and page 3
    page 2 for choosing pre built vs pre construction
    
    page 3 for choosing condo vs house

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import "./App.css";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import House from "./Pages/House";
+import Page2 from "./Pages/Page2";
+import Page3 from "./Pages/Page3";
+
+
 
 function App() {
   return (
@@ -12,6 +16,8 @@ function App() {
       <Route path="/" exact component={Home} />
       <Route path="/about" exact component={About} />
       <Route path="/house" exact component={House} />
+      <Route path="/type-built" exact component={Page2} />
+      <Route path="/type-house" exact component={Page3} />
     </BrowserRouter>
   );
 }
