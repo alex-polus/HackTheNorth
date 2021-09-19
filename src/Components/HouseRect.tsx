296521cfd9580a331c326622731278f4c2a89bbf
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2021-09-18 23:50:31 -0400

    add doughnuts checkpoint

--- src/Components/HouseRect.tsx
+++ src/Components/HouseRect.tsx
@@ -1,9 +1,35 @@
 import { Divider, Grid } from "@mui/material";
 import Box from "@mui/material/Box";
+import { Doughnut } from "react-chartjs-2";
 
 import React from "react";
 import "./HouseRect.css";
-
+const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 function HouseRect() {
   return (
     <Box
@@ -27,7 +53,9 @@ function HouseRect() {
           <p>based on average price of current listings</p>
         </Grid>
         <Divider orientation="vertical" flexItem />
-        <Grid item></Grid>
+        <Grid item>
+          <Doughnut data={data} />
+        </Grid>
       </Grid>
     </Box>
   );
