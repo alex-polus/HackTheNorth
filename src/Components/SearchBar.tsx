926aa383be4d39f8e9937c10f0b9d4f592ee9232
JugalC <44619729+JugalC@users.noreply.github.com>
2021-09-19 00:44:57 -0400

    some progress on page 2

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -3,14 +3,27 @@ import React from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+
+  function handleSubmit() {
+    // event.preventDefault();
+    console.log("hi")
+    
+    // You should see email and password in console.
+    // ..code to submit form to backend here...
+
+  }
+
+
   return (
-    <TextField
-      className="searchBar"
-      id="outlined-basic"
-      label="Enter an address, neighbourhood, city or ZIP code."
-      variant="outlined"
-      color="primary"
-    />
+    <form onSubmit={handleSubmit} >
+      <TextField
+        className="searchBar"
+        id="outlined-basic"
+        label="Enter an address, neighbourhood, city or ZIP code."
+        variant="outlined"
+        color="primary"
+      />
+    </form>
   );
 }
 
--- src/Pages/Page2.tsx
+++ src/Pages/Page2.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
+import { Box, Grid, Paper } from "@mui/material";
+import BoxCard from "../Components/BoxCard";
+
+let height = {
+  height: 550,
+};
+const pre_built =
+  "https://raw.githubusercontent.com/alex-polus/HackTheNorth/client-web/src/img/prebuilt.png";
+const pre_construct =
+  "https://raw.githubusercontent.com/alex-polus/HackTheNorth/client-web/src/img/preconstruct.png";
 
 function Page2() {
-  return <div>Page2</div>;
+  return (<div>
+    <Navbar />
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      style={height}
+    >
+
+      <Grid item width="60%">
+        <h2 style={{ textAlign: "center" }}>Pre-built, or pre-construction?</h2>
+      </Grid>
+      <Grid item width="60%">
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="baseline"
+          spacing={0}
+        >
+          <BoxCard
+            imgName={pre_built}
+            imgWidth={245}
+            imgHeight={181}
+            titleCard={"Pre-Built"}
+            titleDesc={
+              "I want a home that's already been built"
+            }
+          />
+          <BoxCard
+            imgName={pre_construct}
+            imgWidth={263}
+            imgHeight={126}
+            titleCard={"Pre-Construction"}
+            titleDesc={
+              "I'm looking for a pre-construction"
+            }
+          />
+
+        </Grid>
+      </Grid>
+    </Grid>
+  </div>);
 }
 
 export default Page2;
