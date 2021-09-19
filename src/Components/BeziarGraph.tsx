6d559d58785741ca153ff71fa203053df087da1c
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2021-09-19 10:47:25 -0400

    add image

--- src/Components/BeziarGraph.tsx
+++ src/Components/BeziarGraph.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+const image = "";
 export default function BeziarGraph() {
-  return <div></div>;
+  return (
+    <div>
+      <img src={image}></img>
+    </div>
+  );
 }
